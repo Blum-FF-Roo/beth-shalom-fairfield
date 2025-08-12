b74ba42163788d02bcb3c492cf7205689cd07769
aaron blum <blumaa@gmail.com>
2025-08-12 22:59:27 +0200

    remove admin

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -71,9 +71,6 @@ export default function LoginPage() {
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Login
         </h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Sign in to access the admin panel
-        </p>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
